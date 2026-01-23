b592163f7f7edb65eac085a247bc73fb573ee0bb
Brett Johnson <johnson.brett@bwjsoftware.com>
2026-01-22 19:22:07 -0600

    Created basic template for Projects

---
+++ src/components/Project.vue
@@ -0,0 +1,34 @@
+<script setup>
+	const props = defineProps({
+	title: {
+		type: String,
+		default: '',
+	},
+	description: {
+		type: String,
+		default: "Description Unavailable",
+	},
+	pdflink: {
+		type: String,
+		default: undefined,
+	},
+	githublink: {
+		type: String,
+		default: undefined,
+	},
+
+	});
+</script>
+
+<template>
+	<div>
+		<div>
+			
+		</div>
+		<div>
+			<h2><a :href="pdflink">{{ title }}</a></h2>
+			<p>{{ description }}</p>
+			<p v-if="githublink">Open in Github: <a :href="githublink" target="_blank" rel="noopener noreferrer">{{ githublink }}</a></p>
+		</div>
+	</div>
+</template>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+	import Project from '@/components/Project.vue';
 </script>
 
 <template>
-	<div class="text-center">
-		<h1 class="text-5xl text-tertiary">Under Construction</h1>
-		<h1 class="text-5xl text-tertiary">Coming Soon...</h1>
+	<div class="text-center text-tertiary">
+		<h1 class="text-3xl">OS2G Technical Talks</h1>
+		<Project title="Test" pdflink="/resume.pdf" />
+		<h1 class="text-3xl">Check Back Later For More</h1>
 	</div>
 </template>
