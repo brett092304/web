244cba60bf51960819d2753d57a041491ba83365
Brett Johnson <johnson.brett@bwjsoftware.com>
2026-01-22 20:42:26 -0600

    Add os2g project talk about virtualization - finished project templates for the most part

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -12,6 +12,10 @@
 		type: String,
 		default: undefined,
 	},
+	pdfimage: {
+		type: String,
+		default: undefined,
+	},
 	githublink: {
 		type: String,
 		default: undefined,
@@ -21,13 +25,13 @@
 </script>
 
 <template>
-	<div>
-		<div>
-			
+	<div class="grid grid-cols-6 gap-4 m-[25px]">
+		<div v-if="pdfimage" class="col-span-1">
+			<a :href="pdflink" target="_blank" rel="noopener noreferrer"><img :src="`/img/${pdfimage}`" alt="Project Image" class="rounded-2xl" /></a>
 		</div>
-		<div>
-			<h2><a :href="pdflink">{{ title }}</a></h2>
-			<p>{{ description }}</p>
+		<div class="col-span-3 justify-left">
+			<h2 class="text-xl"><a :href="pdflink" target="_blank" rel="noopener noreferrer">{{ title }}</a></h2>
+			<p><a :href="pdflink" target="_blank" rel="noopener noreferrer">{{ description }}</a></p>
 			<p v-if="githublink">Open in Github: <a :href="githublink" target="_blank" rel="noopener noreferrer">{{ githublink }}</a></p>
 		</div>
 	</div>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-	<div class="text-center text-tertiary">
-		<h1 class="text-3xl">OS2G Technical Talks</h1>
-		<Project title="Test" pdflink="/resume.pdf" />
-		<h1 class="text-3xl">Check Back Later For More</h1>
+	<div class="text-tertiary">
+		<h1 class="text-center text-3xl">OS2G Technical Talks</h1>
+		<Project title="Virtualization" pdflink="/project-pdfs/os2g-talk-virtualization.pdf" pdfimage="os2g-talk-virtualization.png"  description="A Technical Talk about getting started with Virtualization technology via 'VirtualBox'" />
+		<h1 class="text-center text-3xl">Check Back Later For More</h1>
 	</div>
 </template>
